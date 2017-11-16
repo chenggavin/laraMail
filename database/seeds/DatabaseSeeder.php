748addd5b3d766dbb60e64050f05e8fe0935e999
caseydemo <cdemattina@gmail.com>
2017-11-16 10:49:25 -0500

    got rid of database seeds call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(MessageSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(MessageSeeder::class);
     }
 }
