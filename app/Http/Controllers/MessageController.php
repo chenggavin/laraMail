ab2ae275aa454696f626b0728169491c402e9ee3
harrisonhadaway <harrisonleehadaway@gmail.com>
2017-11-17 14:51:54 -0500

    issue 17 is a done deal

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -306,4 +306,13 @@ class MessageController extends Controller
 
     }
 
+    public function starInbox($id) 
+    {
+        $message = \App\Message::find($id);
+        $recipient = $message->recipients->find(\Auth::user()->id);
+        $message->recipients()->updateExistingPivot(\Auth::user()->id, ['is_starred' => !$recipient->pivot->is_starred]);
+        return redirect('/messages');
+
+    }
+
 }
