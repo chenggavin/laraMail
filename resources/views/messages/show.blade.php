109c91c0f442a2cb6e90eed5f74c8850011bcce6
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-21 11:04:34 -0500

    fixed a bug: the reply button wasn't showing when receiving a message with multiple recipients

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -130,11 +130,12 @@ On {{ $message->prettySent() }}, {{ $message->sender()->first()->name }} wrote:
       </div>
       <div class="form-group">
 
-        @if (count($message->recipients()->get()) > 1)
-          @if ($message->sender_id !== \Auth::user()->id)
-             <button type="submit" name="button" value="replyOne" class="btn btn-primary">Reply</button>
-          @endif
+        
+        @if (count($message->recipients()->get()) > 1 && url()->previous() !== url("messages/sent"))
+            <button type="submit" name="button" value="replyOne" class="btn btn-primary">Reply</button>
             <button type="submit" name="button" value="replyAll" class="btn btn-primary">Reply All</button>
+        @elseif (url()->previous() === url("messages/sent") && count($message->recipients()->get()) > 1)
+           <button type="submit" name="button" value="replyAll" class="btn btn-primary">Reply All</button>
         @else
             <button type="submit" name="button" value="replyOne" class="btn btn-primary">Reply</button>
         @endif
